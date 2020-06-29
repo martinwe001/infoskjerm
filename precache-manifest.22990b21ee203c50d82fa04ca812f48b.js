5b62a1870a71b6223220c1fa16e2812e090bac8b
Martin Wangen-Eriksen <martinwe@Martins-MacBook-Pro.local>
2020-06-29 13:42:47 +0200

    Updates

---
+++ precache-manifest.22990b21ee203c50d82fa04ca812f48b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9166d274ec3a0382f0854ca2275f3bfe",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "66bcf233558875369864",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ffc05c13f9a7c5d7f1fa",
+    "url": "/infoskjerm/static/js/2.e7d29533.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.e7d29533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bcf233558875369864",
+    "url": "/infoskjerm/static/js/main.5b7a0c7f.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
