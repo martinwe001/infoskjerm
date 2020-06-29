3b623b53063eb013f300000b5e50b8955aa1df5d
Martin Wangen-Eriksen <martinwe@Martins-MacBook-Pro.local>
2020-06-29 14:18:43 +0200

    add clock2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Reddit from './Reddit';
 import Bikes from './Bikes';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
+      <Clock/>
       <Reddit />
       <Bikes />
     </div>
---
+++ src/Clock.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {DateTime} from 'luxon';
+
+
+
+const Clock = () => {
+    const dateTime = DateTime.local().setLocale("nb-NO");
+
+    console.log(DateTime.TIME_24_SIMPLE);
+
+    return (
+    <div className="Clock">
+        <h1 className="time">
+            {dateTime.toLocaleString(DateTime.TIME_24_SIMPLE)}
+        </h1>
+        <h1 className="date">
+            {dateTime.toLocaleString({
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+            })}
+        </h1>
+
+
+    </div>
+    )
+};
+
+
+export default Clock;
